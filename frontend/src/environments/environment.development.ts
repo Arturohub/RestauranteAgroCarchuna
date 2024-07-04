b4b0982edee1a9da32edb5d24ff1ace629ffa392
Arturo Chica Villa <arturochicavilla@hotmail.com>
2024-07-04 09:49:29 +0200

    Docker changes

--- frontend/src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    baseUrl: "https://restaurante-agrocarchuna-api.onrender.com/api",
-    imageUrl: "https://api.cloudinary.com/v1_1/dws8qazyi/image/upload",
-    imagePass: "xcko2p8s",
-    authUrl: "https://restaurante-agrocarchuna-api.onrender.com/api/auth",
-    tokenName: "miniArturoToken"
-};
