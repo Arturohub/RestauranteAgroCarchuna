a385dedbaf3fd028bd71e59f88065733aeec54e7
Arturo Chica Villa <arturochicavilla@hotmail.com>
2024-07-04 10:21:07 +0200

    Docker changes

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    baseUrl: "http://localhost:8080/api",
+    imageUrl: "https://api.cloudinary.com/v1_1/dws8qazyi/image/upload",
+    imagePass: "xcko2p8s",
+    authUrl: "http://localhost:8080/api/auth",
+    tokenName: "miniArturoToken"
+};
