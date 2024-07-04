168849386b0a13146140753f7a55ffcc66a8a005
Arturo Chica Villa <arturochicavilla@hotmail.com>
2024-07-04 09:09:51 +0200

    Updates for deployment

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    baseUrl: "http://localhost:8080/api",
+    baseUrl: "https://restaurante-agrocarchuna-api.onrender.com/api",
     imageUrl: "https://api.cloudinary.com/v1_1/dws8qazyi/image/upload",
     imagePass: "xcko2p8s",
-    authUrl: "http://localhost:8080/api/auth",
+    authUrl: "https://restaurante-agrocarchuna-api.onrender.com/api/auth",
     tokenName: "miniArturoToken"
 };
