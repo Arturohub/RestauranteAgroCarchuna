e0e628fc6c8e4bad100e211f4e3c7fed5f91099d
Arturo Chica Villa <arturochicavilla@hotmail.com>
2024-07-09 11:25:38 +0200

    cors issue

--- backend/src/main/java/com/arturo/backend/security/WebSecurityConfig.java
+++ backend/src/main/java/com/arturo/backend/security/WebSecurityConfig.java
@@ -79,9 +79,9 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
       CorsConfiguration configuration = new CorsConfiguration();
-      configuration.setAllowedOrigins(Arrays.asList("*"));
+      configuration.setAllowedOrigins(List.of("https://restauranteagrocarchuna.onrender.com"));
       configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-      configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+      configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Access-Control-Allow-Origin"));
       configuration.setAllowCredentials(true);
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       source.registerCorsConfiguration("/**", configuration);
